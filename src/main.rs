c2da8e4b67eade4ffde0abad2d50f7b44c8eecba
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-07-03 14:07:37 -0500

    Code review from Rust Club

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,68 @@
+use std::fmt::Display;
+
+use anyhow::anyhow;
 use clap::Parser;
 
+#[derive(Parser, Clone, Debug)]
 struct Point {
     x: f32,
-    y: f32
+    y: f32,
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = format!("{},{}", self.x, self.y);
+        s.fmt(f)
+    }
+}
+
+impl std::str::FromStr for Point {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let Some((l, r)) = s.split_once(',') else {
+            anyhow::bail!("Point has a number, then a comma, then another number")
+        };
+        let x = l
+            .parse()
+            .map_err(|e| anyhow!("Could not parse a number: {e}"))?;
+        let y = r.parse()?;
+        Ok(Point { x, y })
+    }
 }
 
 /// Simple program to measure a line
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct MeasureApp {
-    /// X-value of start of line to measure
+    /// First point
     #[arg(long)]
-    x1: f32,
+    start: Point,
 
-    /// Y-value of start of line to measure
+    /// Second point
     #[arg(long)]
-    y1: f32,
-
-    /// X-value of end of line to measure
-    #[arg(long)]
-    x2: f32,
-
-    /// Y-value of end of line to measure
-    #[arg(long)]
-    y2: f32
+    end: Point,
 }
 
 fn main() {
     let args = MeasureApp::parse();
 
-    println!("Start: x = {}, y = {}", args.x1, args.y1);
-    println!("End: x = {}, y = {}", args.x2, args.y2);
-    
-    let point_1 = Point { x: args.x1, y: args.y1 };
-    let point_2 = Point { x: args.x2, y: args.y2 };
-    let distance = distance_between_points(point_1, point_2);
+    println!("Start: {}", args.start);
+    println!("End: {}", args.end);
+
+    let distance = distance_between_points(args.start, args.end);
 
     println!("Distance between points: {}", distance);
 }
 
 /// Calculate the distance between two points
-fn distance_between_points(point_1: Point, point_2: Point) -> f32 {
-    let x_diff = point_1.x - point_2.x;
-    let y_diff = point_1.y - point_2.y;
-    let x_squared = x_diff.powi(2);
-    let y_squared = y_diff.powi(2);
+#[rustfmt::skip]
+fn distance_between_points(
+    Point { x: x1, y: y1 }: Point, 
+    Point { x: x2, y: y2 }: Point,
+) -> f32 {
+    let x_squared = (x1 - x2).powi(2);
+    let y_squared = (y1 - y2).powi(2);
     let sum = x_squared + y_squared;
     let distance = sum.sqrt();
     distance
